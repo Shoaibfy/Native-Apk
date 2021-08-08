ca525388639d5006ecf3ced5f801de89479ee618
shoaibfy <Shoaib5023@gmail.com>
2021-08-09 01:42:38 +0530

    copilot step

--- eCommerce/Components/CopilotScreen.js
+++ eCommerce/Components/CopilotScreen.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {copilot, walkthroughable, CopilotStep} from 'react-native-copilot';
+import React, {Component} from 'react';
+import {View, Text, StatusBar} from 'react-native';
 
-const CopilotScreen = () => {
-  return (
-    <View>
-      <Text>Copilot</Text>
-    </View>
-  );
-};
+const CopilotText = walkthroughable(Text);
 
-export default CopilotScreen;
+class HomeScreen extends Component {
+  componentDidMount() {
+    this.props.start();
+  }
+
+  componentWillUnmount() {
+    this.props.copilotEvents.off('stop');
+  }
+
+  render() {
+    return (
+      <View>
+        <CopilotStep
+          text="This is a hello world example!"
+          order={1}
+          name="hello">
+          <CopilotText>Hello world!</CopilotText>
+        </CopilotStep>
+      </View>
+    );
+  }
+}
+
+export default copilot({
+  overlay: 'svg', // or 'view'
+  animated: true,
+})(HomeScreen);
