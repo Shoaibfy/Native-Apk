4575a109dde1434ab242462a7152f51a28296117
shoaibfy <Shoaib5023@gmail.com>
2021-08-09 02:11:02 +0530

    step 2 in Copilot

--- eCommerce/Components/CopilotScreen.js
+++ eCommerce/Components/CopilotScreen.js
@@ -1,10 +1,19 @@
 import {copilot, walkthroughable, CopilotStep} from 'react-native-copilot';
 import React, {Component} from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, Image, Switch} from 'react-native';
 
 const CopilotText = walkthroughable(Text);
+const WopilotText = walkthroughable(Image);
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondStepActive: true,
+    };
+  }
+
   componentDidMount() {
     this.props.start();
   }
@@ -16,12 +25,21 @@ class HomeScreen extends Component {
   render() {
     return (
       <View>
-        <CopilotStep
-          text="This is a hello world example!"
-          order={1}
-          name="hello">
-          <CopilotText>Hello world!</CopilotText>
-        </CopilotStep>
+        <View>
+          <CopilotStep
+            style={{marginTop: 20}}
+            text="This is a hello world example!"
+            order={1}
+            name="hello">
+            <CopilotText>Hello world!</CopilotText>
+          </CopilotStep>
+        </View>
+        <View>
+          <CopilotStep text="This is a heaven" order={2} name="hello2">
+            <CopilotText>Heaven !</CopilotText>
+          </CopilotStep>
+        </View>
+        <Switch onValueChange={this.state.secondStepActive} />
       </View>
     );
   }
